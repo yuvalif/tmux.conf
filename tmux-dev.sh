e5bd51b83d171d84ffcb5a1f0f17c8592838acf9
Yuval Lifshitz <yuvalif@yahoo.com>
2020-02-14 10:27:11 +0200

    add copy&paste. add "usage"
    Signed-off-by: Yuval Lifshitz <yuvalif@yahoo.com>

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-cd $2
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <session name> <directory name>"
+    exit 1
+fi
+
+cd "$2" || exit 1
 
 SESSION=$1
 
-tmux -2 new-session -d -s $SESSION
+tmux -2 new-session -d -s "$SESSION"
 
-tmux new-window -t $SESSION:1 
-tmux new-window -t $SESSION:2
-tmux new-window -t $SESSION:3
+tmux new-window -t "$SESSION":1 
+tmux new-window -t "$SESSION":2
+tmux new-window -t "$SESSION":3
+tmux new-window -t "$SESSION":4
 
 # Set default window
-tmux select-window -t $SESSION:1
+tmux select-window -t "$SESSION":1
 
 # Attach to session
-tmux -2 attach-session -t $SESSION
+tmux -2 attach-session -t "$SESSION"
